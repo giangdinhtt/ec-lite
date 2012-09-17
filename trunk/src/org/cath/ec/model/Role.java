317f7160ba505b1f1c64da0dbc45884bf41ef093
giangdinhtt@gmail.com <giangdinhtt@gmail.com@05c30de2-7cf8-65ce-ca05-17812a7bac8e>
2012-09-17 18:58:50 +0000

    EC enhancements

---
+++ trunk/src/org/cath/ec/model/Role.java
@@ -0,0 +1,71 @@
+package org.cath.ec.model;
+
+import java.util.Set;
+
+/**
+ * Model object that represents a security role.
+ */
+/*@Entity
+@Table(name="roles")
+@Cache(usage= CacheConcurrencyStrategy.READ_WRITE)*/
+public class Role {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Set<String> permissions;
+/*
+    protected Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic(optional=false)
+    @Column(length=100)
+    @Index(name="idx_roles_name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic(optional=false)
+    @Column(length=255)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @CollectionOfElements
+    @JoinTable(name="roles_permissions")
+    @Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+    public Set<String> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Set<String> permissions) {
+        this.permissions = permissions;
+    }*/
+
+}
