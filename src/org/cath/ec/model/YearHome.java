476663aa9dceda0007fb50133a47c61e24c32ecf
giangdinhtt <giangdinhtt@gmail.com>
2012-09-18 19:06:47 +0000

    Added Hibernate mapping files and model

---
+++ src/org/cath/ec/model/YearHome.java
@@ -0,0 +1,117 @@
+package org.cath.ec.model;
+
+// Generated Sep 19, 2012 12:31:24 AM by Hibernate Tools 3.4.0.CR1
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import static org.hibernate.criterion.Example.create;
+
+/**
+ * Home object for domain model class Year.
+ * @see org.cath.ec.model.Year
+ * @author Hibernate Tools
+ */
+public class YearHome {
+
+    private static final Log log = LogFactory.getLog(YearHome.class);
+
+    private final SessionFactory sessionFactory = getSessionFactory();
+
+    protected SessionFactory getSessionFactory() {
+        try {
+            return (SessionFactory) new InitialContext().lookup("SessionFactory");
+        } catch (Exception e) {
+            log.error("Could not locate SessionFactory in JNDI", e);
+            throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+        }
+    }
+
+    public void persist(Year transientInstance) {
+        log.debug("persisting Year instance");
+        try {
+            sessionFactory.getCurrentSession().persist(transientInstance);
+            log.debug("persist successful");
+        } catch (RuntimeException re) {
+            log.error("persist failed", re);
+            throw re;
+        }
+    }
+
+    public void attachDirty(Year instance) {
+        log.debug("attaching dirty Year instance");
+        try {
+            sessionFactory.getCurrentSession().saveOrUpdate(instance);
+            log.debug("attach successful");
+        } catch (RuntimeException re) {
+            log.error("attach failed", re);
+            throw re;
+        }
+    }
+
+    public void attachClean(Year instance) {
+        log.debug("attaching clean Year instance");
+        try {
+            sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+            log.debug("attach successful");
+        } catch (RuntimeException re) {
+            log.error("attach failed", re);
+            throw re;
+        }
+    }
+
+    public void delete(Year persistentInstance) {
+        log.debug("deleting Year instance");
+        try {
+            sessionFactory.getCurrentSession().delete(persistentInstance);
+            log.debug("delete successful");
+        } catch (RuntimeException re) {
+            log.error("delete failed", re);
+            throw re;
+        }
+    }
+
+    public Year merge(Year detachedInstance) {
+        log.debug("merging Year instance");
+        try {
+            Year result = (Year) sessionFactory.getCurrentSession().merge(detachedInstance);
+            log.debug("merge successful");
+            return result;
+        } catch (RuntimeException re) {
+            log.error("merge failed", re);
+            throw re;
+        }
+    }
+
+    public Year findById(java.lang.Integer id) {
+        log.debug("getting Year instance with id: " + id);
+        try {
+            Year instance = (Year) sessionFactory.getCurrentSession().get("org.cath.ec.model.Year", id);
+            if (instance == null) {
+                log.debug("get successful, no instance found");
+            } else {
+                log.debug("get successful, instance found");
+            }
+            return instance;
+        } catch (RuntimeException re) {
+            log.error("get failed", re);
+            throw re;
+        }
+    }
+
+    public List<Year> findByExample(Year instance) {
+        log.debug("finding Year instance by example");
+        try {
+            List<Year> results = (List<Year>) sessionFactory.getCurrentSession()
+                    .createCriteria("org.cath.ec.model.Year").add(create(instance)).list();
+            log.debug("find by example successful, result size: " + results.size());
+            return results;
+        } catch (RuntimeException re) {
+            log.error("find by example failed", re);
+            throw re;
+        }
+    }
+}
